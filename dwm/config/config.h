2f482a157f4eae4897904d1cf5dc3dd4fd16ff66
Kied Llaentenn <32681240+Kiedtl@users.noreply.github.com>
2019-12-15 08:37:47 -0500

    fix configs to match rice

--- dwm/config/config.h
+++ dwm/config/config.h
@@ -1,5 +1,3 @@
-#include "/home/kiedtl/.cache/wal/colors-wal-dwm.h"
-
 static unsigned int gappx = 12;
 static const unsigned int borderpx  = 2;
 static const int showbar            = 1;
@@ -7,37 +5,12 @@ static const int topbar             = 1;
 static const int bh = 29; // bar height
 static const int focusonwheel = 1;
 
-//static const char *colors[][3]      = {
-//    /*               fg         bg         border   */
-//    [SchemeNorm] = { "#bbbbbb", "#222222", "#555555" },
-//    [SchemeSel]  = { "#eeeeee", "#eeeeee", "#333333" },
-//    [SchemeUrg]  = { "#ff0000", "#ff0000", "#ff0000" },
-//};
-
-/* my colors */
-/*static const char col_norm_fg[]     = "#cacbca";
-static const char col_norm_bg[]     = "#030f11";
-static const char col_sel_fg[]      = "#cacbca";
-
-static const char col_sel_bg[]      = "#030f11";
-static const char col_norm_bdr[]    = "#8d8e8d";
-static const char col_sel_bdr[]     = "#cacbca";
-
-static const char norm_fg[] = "#cacbca";
-static const char norm_bg[] = "#030F11";
-static const char norm_border[] = "#8d8e8d";
-
-static const char sel_fg[] = "#cacbca";
-static const char sel_bg[] = "#4B5451";
-static const char sel_border[] = "#cacbca";
-
-* end my colors *
 static const char *colors[][3]      = {
-	[SchemeNorm] = { col_norm_fg, col_norm_bg, col_norm_bdr },
-	[SchemeSel]  = { col_sel_fg, col_sel_bg,  col_sel_bdr },
-	[SchemeUrg]  = { col_sel_fg, col_sel_bg,  col_sel_bdr },
+    /*               fg         bg         border   */
+    [SchemeNorm] = { "#c3d3d2", "#080807", "#9a4f31" }, // unfocused windows
+    [SchemeSel]  = { "#c3d3d2", "#685445", "#a3d3d2" }, // focused window
+    [SchemeUrg]  = { "#c3d3d2", "#2d4e42", "#2d4e42" },
 };
-*/
 
 static const int NUM_WORKSPACES=9;
 static const char *tags[] = { "0", "1", "2", "3", "4", "5", "6", "7", "8" };
@@ -45,6 +18,7 @@ static const char *tags[] = { "0", "1", "2", "3", "4", "5", "6", "7", "8" };
 static const Rule rules[] = {
     /* class      instance    title                 tags mask  iscentered   isfloating   monitor */
     { "feh",             NULL,       "feh",               0,         1,           1,           -1 },
+    { "xclock",          NULL,       NULL,                0,         1,           1,           -1 },
 };
 
 static const float mfact     = 0.5;
@@ -66,21 +40,16 @@ static const Layout layouts[] = {
 
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 
-// old dmenu
-// static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", "xos4 Terminus:size=11", "-nb", col_norm_bg, "-nf", col_norm_fg, "-sb", col_sel_bg, "-sf", col_sel_fg, NULL };
-
 // new dmenu
-static const char *dmenucmd[] = { "ndmen", NULL};
 static const char *termcmd[]  = { "xterm", "-title", "terminal", NULL};
-static const char *lockcmd[]  = { "slock", NULL};
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "xterm", "-title", scratchpadname, "-geometry", "84x24", NULL };
  
 static Key keys[] = {
     /* modifier                     key        function        argument */
-    { MODKEY,                       XK_p,      spawn,          {.v = dmenucmd }  },
+    { MODKEY,                       XK_p,      spawn,          SHCMD("ndmen") },
     { MODKEY,                       XK_Return, spawn,          {.v = termcmd }  },
-    { MODKEY|ShiftMask,             XK_s,      spawn,          {.v = lockcmd }  },
+    { MODKEY|ShiftMask,             XK_s,      spawn,          SHCMD("slock") },
     { MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
     { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
     { MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
--- slock/config/config.h
+++ slock/config/config.h
@@ -3,10 +3,10 @@ static const char *user  = "kiedtl";
 static const char *group = "kiedtl";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "#FFFFFF",   /* after initialization */
+	[INIT] =   "#080808",   /* after initialization */
 	[INPUT] =  "#0A96FF",   /* during input */
 	[FAILED] = "#FF0A0A",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
-static const int failonclear = 1;
+static const int failonclear = 0;
