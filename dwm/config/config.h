5064216fbfde0ec9166381c03dc33f36106dccc2
Kiëd Llaentenn <32681240+kiedtl@users.noreply.github.com>
2020-01-06 20:33:27 -0500

    new rice

--- dwm/config/config.h
+++ dwm/config/config.h
@@ -1,15 +1,29 @@
-static unsigned int gappx = 16;
-static const unsigned int borderpx  = 2;
-static const int showbar            = 1;
-static const int topbar             = 1;
-static const int bh = 25; // bar height
-static const int focusonwheel = 1;
+/*
+ *
+ * as you may have noticed, I'm one of those maniacs
+ * who likes to align everything :P
+ *
+*/
+
+#include "fibonacci.c"
+
+static       unsigned int gappx        = 16;
+static const unsigned int borderpx     = 2;
+static const int          showbar      = 1;
+static const int          topbar       = 1;
+static       int          bh           = 25; // bar height
+static const int          horizpadbar  = 2;
+static const int          vertpadbar   = 0;
+static const char         *fonts[]     = { "ttyp0:size=15" };
+
+static const char         background[] = "#0e120d";
+static const char         foreground[] = "#e1e2e2";
 
 static const char *colors[][3]      = {
-    /*               fg         bg         border   */
-    [SchemeNorm] = { "#c3d3d2", "#080807", "#0e120d" }, // unfocused windows
-    [SchemeSel]  = { "#c3d3d2", "#685445", "#e1e2e2" }, // focused window
-    [SchemeUrg]  = { "#c3d3d2", "#2d4e42", "#2d4e42" },
+    /*               fg          bg          border   */
+    [SchemeNorm] = { foreground, background, background }, /* no border for unfocused windows */
+    [SchemeSel]  = { foreground, background, foreground }, /* focused window */
+    [SchemeUrg]  = { foreground, background, "#2d4e42"  },
 };
 
 static const int NUM_WORKSPACES=9;
@@ -21,9 +35,9 @@ static const Rule rules[] = {
     { "xclock",          NULL,       NULL,                0,         1,           1,           -1 },
 };
 
-static const float mfact     = 0.5;
-static const int nmaster     = 1;
-static const int resizehints = 1;
+static const float mfact       = 0.5; /* factor of master artea size [0.05..0.95] */
+static const int   nmaster     = 1;   /* number of clients in master area */
+static const int   resizehints = 1;   /* 1 = respect size hints in tiles resizals */
 
 static const int NUM_LAYOUTS = 3;
 static const Layout layouts[] = {
@@ -31,6 +45,8 @@ static const Layout layouts[] = {
     { "[]=",      tile },    /* first entry is default */
     { "><>",      NULL },    /* NULL arrange function = floating */
     { "[M]",      monocle },
+    { "(@)",      spiral },
+    { "[\\]",     dwindle },
 };
 
 #define MODKEY Mod4Mask
@@ -71,7 +87,10 @@ static Key keys[] = {
 };
 
 static Button buttons[] = {
+    { ClkTagBar,            0,              Button1,        view,           {0} },
+    { ClkTagBar,            0,              Button3,        toggleview,     {0} },
+    { ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+    { ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
     { ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
     { ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
-    //{ ClkRootWin,           0,              Button3,        spawn,          SHCMD("/home/kiedtl/bin/rcmen") },
 };
